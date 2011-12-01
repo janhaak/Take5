13970ffc1c33c9ca630a6154cfc7d7d8c4d5896b
Scott Gerring <scott@scottgerring.com>
2011-12-01 12:52:47 +0800

    Added Take5 iphone app ui stubs

---
+++ iphone/Take5/Take5Tests/Take5Tests.h
@@ -0,0 +1,13 @@
+//
+//  Take5Tests.h
+//  Take5Tests
+//
+//  Created by Scott Gerring on 1/12/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface Take5Tests : SenTestCase
+
+@end
---
+++ iphone/Take5/Take5Tests/Take5Tests.m
@@ -0,0 +1,32 @@
+//
+//  Take5Tests.m
+//  Take5Tests
+//
+//  Created by Scott Gerring on 1/12/11.
+//  Copyright (c) 2011 __MyCompanyName__. All rights reserved.
+//
+
+#import "Take5Tests.h"
+
+@implementation Take5Tests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in Take5Tests");
+}
+
+@end
